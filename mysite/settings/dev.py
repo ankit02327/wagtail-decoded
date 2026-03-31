4640ccecc065ea3c16a28aa888d740d315c2d209
ankit02327 <ankit2327@protonmail.com>
2026-03-31 16:44:46 +0530

    Initial setup for site

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-l6(+-k*(9qt*87zfq5a9kk5a9jy&o1)-(8uo0v&ux^!7b^kg@^"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
